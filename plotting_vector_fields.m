07bc55ccde9e3563a62eb8ed96bc84ba22f4e2b1
Jai Israni <55907204+jaiisrani@users.noreply.github.com>
2022-11-26 14:20:53 +0100

    Add files via upload

---
+++ plotting_vector_fields.m
@@ -0,0 +1,24 @@
+X = [ 0 ];
+Y = [ 0 ];
+Z = [ 0 ];
+
+for x = [ -3:2:3 ]
+    for y = [ -3:2:3 ]
+        for z = [ -3:2:3 ]
+            X = [X, x]; % this is to append
+            Y = [Y, y];     % the new coordinate
+            Z = [Z, z];     % to the list of coords
+        end
+    end
+end
+
+B0 = 1; L =1;
+
+Bx = -(B0 / L^2) * Z .* X;
+By = -(B0 / L^2) * Z .* Y;
+Bz = 0 * X;
+
+q = quiver3(X, Y, Z, Bx, By, Bz);
+axis equal;
+q.ShowArrowHead = 'off';
+q.Marker = '.';
