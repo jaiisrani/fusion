70938c4b57eb5ac8b99643c565f829c050931b71
Jai Israni <55907204+jaiisrani@users.noreply.github.com>
2023-03-09 07:55:24 +0100

    Add files via upload

--- creating_matrix1.m
+++ creating_matrix1.m
@@ -8,12 +8,12 @@
 
 training_data = zeros(3, 3000);
 for i = 1 : 1 : 3000
-    training_data(:, i) = [1.*i/3000 2.*i/3000 3.*i/3000];
+    training_data(:, i) = [1.*(i/3000)^2 2.*(i/3000)^2 3.*(i/3000)^2];
 end
 
 % Testing dataset
 
 testing_data = zeros(3, 1);
-testing_data(:, 1) = [0.2, 0.4, 0.8];
+testing_data(:, 1) = [0.20, 0.55, 0.75]; % corresponding to i=1500
 
 save("datasets.mat", "training_data", "testing_data");
